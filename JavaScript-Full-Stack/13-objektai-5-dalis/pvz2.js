8ccc04d8798c88e52480d0af6c958735a4283fb1
Bizuunas <kostas.bizevicius@gmail.com>
2023-12-13 14:22:23 +0200

    -

---
+++ JavaScript-Full-Stack/13-objektai-5-dalis/pvz2.js
@@ -0,0 +1,13 @@
+let skaiciai = [3, 8, 5, 6, 9, 3, 10, 5];
+
+// 1 metodas
+
+let didziausias = skaiciai[0];
+
+for (const skaicius of skaiciai) {
+    if (skaicius > didziausias) {
+        didziausias = skaicius;
+    }
+}
+
+console.log('didziausias skaicius:', didziausias);
