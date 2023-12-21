ed7257b2c7708ebbec760fb593145f77c3f3c751
Bizuunas <kostas.bizevicius@gmail.com>
2023-12-21 11:59:19 +0200

    -

--- JavaScript-Full-Stack/16-javascript/5-async/scripts.js
+++ JavaScript-Full-Stack/16-javascript/5-async/scripts.js
@@ -10,12 +10,14 @@ mygtukai.forEach((mygtukas) => {
             // console.log("gautas atsakymas:", atsakymas);
             return atsakymas.json();
         }).then(duomenys => {
-            console.log('atsakymas geras:', duomenys);
+            // console.log('atsakymas geras:', duomenys);
             switch (failas) {
                 case 'projektai.json': spausdintiProjektus(duomenys); break;
+                case 'darbuotojai.json': spausdintiDarbuotojus(duomenys); break;
+                case 'uzduotys.json': spausdintiUzduotis(duomenys); break;
             }
         }).catch(klaida => {
-            console.log('atsakymas blogas:', klaida);
+            // console.log('atsakymas blogas:', klaida);
             atsakymoBlokas.innerHTML = `<p>Įvyko klaida.</p>`;
         });
     });
@@ -39,4 +41,36 @@ function spausdintiProjektus(projektai) {
         // html += '</div>';
     }
     atsakymoBlokas.innerHTML = html;
+    atsakymoBlokas.classList.add('projektai');
+    atsakymoBlokas.classList.remove('darbuotojai', 'uzduotys');
+}
+
+function spausdintiDarbuotojus(darbuotojai) {
+    let html = '';
+    for (const darbuotojas of darbuotojai) {
+        // console.log(darbuotojas);
+        html += `<div class="darbuotojas">
+                    <h3>${darbuotojas.vardas} ${darbuotojas.pavarde}</h3>
+                    <p>Dirba ${darbuotojas.etatas} etatu.</p>
+                    <p><strong>Uždirba:</strong> ${darbuotojas.atlyginimas} €/mėn.</p>
+                 </div>`;
+    }
+    atsakymoBlokas.innerHTML = html;
+    atsakymoBlokas.classList.add('darbuotojai');
+    atsakymoBlokas.classList.remove('projektai', 'darbuotojai' );
+}
+
+function spausdintiUzduotis(uzduotys) {
+    let html = '';
+    for (const uzduotis of uzduotys) {
+        html += `<div class="uzduotys">
+        <h3>${uzduotis.uzduotis}</h3>
+        <p><strong>Terminas (iki):</strong> ${uzduotis.terminas}</p>
+        <p><strong>Uzduoties prioritetas:</strong> ${uzduotis.prioritetas}</p>
+        <p><strong>Ar uzduotis baigta: </strong> ${uzduotis.ar_baigta ? 'Taip' : 'Ne'}</p>
+        </div>`;
+    }
+    atsakymoBlokas.innerHTML = html;
+    atsakymoBlokas.classList.add('uzduotys');
+    atsakymoBlokas.classList.remove('projektai', 'darbuotojai' );
 }
